9730cf376a95bb3a128e933ebb683decba6c9959
Son <sonho1230@gmail.com>
2021-12-09 17:52:01 +0700

    khoi tao

---
+++ js/main-func.js
@@ -0,0 +1,90 @@
+/** Bài 1: Quản lý tuyển sinh
+ * Khối 1: Input
+ * score, area, object, test1, test2, test3
+ * Khối 2: 
+ * B1: Tạo biến
+ * B2: Lấy giá trị từ form
+ * B3: Xác định điều kiện và lập công thức tính toán
+ *      1. Kiểm tra điểm nhập vào
+ *      2. Kiểm tra khu vực
+ *      3. Kiểm tra đối tượng
+ * B3: Hiện kết quả
+ * Khối 3: Output
+ *  totalScore, kết quả
+ */
+const area_A = 2;
+const area_B = 1;
+const area_C = 0.5;
+
+const object1 = 2.5;
+const object2 = 1.5;
+const object3 = 1;
+
+function calScore() {
+    var test1 = Number(document.getElementById('inpTest1').value);
+    var test2 = Number(document.getElementById('inpTest2').value);
+    var test3 = Number(document.getElementById('inpTest3').value);
+    var score = Number(document.getElementById('inpScore').value);
+
+    if (score == 0) {
+        document.getElementById('txtThongBao').innerHTML =
+            "Bạn hãy nhập điểm chuẩn ! "
+    }
+    else if (test1 == 0 || test2 == 0 || test3 == 0) {
+        document.getElementById('txtThongBao').innerHTML =
+            "Bạn đã rớt do có điểm 0 !"
+    } else {
+        kiemTraKhuVuc(test1, test2, test3, score)
+    }
+
+}
+function kiemTraKhuVuc(paTest1, paTest2, paTest3, paScore) {
+    var area = document.getElementById('area').value;
+    var totalScore = 0;
+
+    if (area == "A") {
+        totalScore = paTest1 + paTest2 + paTest3 + area_A;
+        kiemTraDoiTuong(totalScore)
+    } else if (area == "B") {
+        totalScore = paTest1 + paTest2 + paTest3 + area_B;
+        kiemTraDoiTuong(totalScore)
+    } else if (area == "C") {
+        totalScore = paTest1 + paTest2 + paTest3 + area_C;
+        kiemTraDoiTuong(totalScore)
+    } else {
+        totalScore = paTest1 + paTest2 + paTest3;
+        kiemTraDoiTuong(totalScore)
+    }
+
+    var kq = kiemTraDoiTuong(totalScore)
+    ketQua(kq, paScore);
+
+
+}
+
+function kiemTraDoiTuong(paTotalScore) {
+    var object = document.getElementById('object').value;
+
+    if (object == "1") {
+        return paTotalScore + object1;
+    } else if (object == "2") {
+        return paTotalScore + object2;
+    } else if (object == "3") {
+        return paTotalScore + object3;
+    } else {
+        return paTotalScore;
+    }
+
+}
+function ketQua(ketQua, paScore) {
+
+    if (ketQua >= paScore) {
+        document.getElementById('txtThongBao').innerHTML =
+            "Bạn đã đậu ! Tổng điểm của bạn là: " + ketQua
+    } else {
+        document.getElementById('txtThongBao').innerHTML =
+            "Bạn đã rớt ! Tổng điểm của bạn là: " + ketQua
+    }
+}
+
+document.getElementById('btnKetQua').onclick = calScore;
---
+++ js/main-func2.js
@@ -0,0 +1,51 @@
+/** Bài 2: Tính tiền điện 
+ * Khối 1: Input
+ * name, soKW
+ * Khối 2: 
+ * B1: Tạo biến
+ * B2: Lấy giá trị từ form
+ * B3: Xác định điều kiện và lập công thức tính toán
+ *   0 < soKW <= 50 => totalMoney = soKW * giaDau
+ *   50 < soKW <= 100 => totalMoney = 50 * giaDau + soKW * gia50_ke
+ *   100 < soKW <= 150 => totalMoney = 50 * giaDau + 50 * gia50_ke + soKW *  gia100_Ke
+ *   100 < soKW <= 150 => totalMoney = 50 * giaDau + 50 * gia50_ke + 100 *  gia100_Ke + soKW *   gia150_Ke
+ *   soKW > 150 => totalMoney = 50 * giaDau + 50 * gia50_ke + 100 *  gia100_Ke + 150 * gia150_Ke +  soKW * giaConLai
+ *      
+ * B3: Hiện kết quả
+ * Khối 3: Output
+ *  totalMoney
+ */
+const giaDau = 500;
+const gia50_Ke = 650;
+const gia100_Ke = 850;
+const gia150_Ke = 1100;
+const giaConLai = 1300;
+function tinhTienDien(){
+    var name = document.getElementById('inpName').value;
+    var soKW = Number(document.getElementById('inpKW').value);
+    var totalMoney = 0;
+
+    if(soKW == 0){
+        document.getElementById('txtTinhTien').innerHTML = "Vui lòng nhập tiền điện !"
+    }else{
+         if(soKW <= 50){
+            totalMoney = soKW * giaDau;
+        }else if( soKW <= 100){
+            totalMoney  = giaDau * 50 + (soKW - 50) * gia50_Ke
+        }else if( soKW <= 200){
+            totalMoney  = giaDau * 50 + gia50_Ke * 50 + (soKW - 100) * gia100_Ke;
+        }else if( soKW <= 350){
+            totalMoney  = giaDau * 50 + gia50_Ke * 50 + gia100_Ke * 100 + (soKW - 200) * gia150_Ke;
+            
+        }else if(soKW > 350){
+            totalMoney  = giaDau * 50 + gia50_Ke * 50 + gia100_Ke * 100 + gia150_Ke * 150 + (soKW - 350) * giaConLai ;  
+        }
+        
+    } 
+       
+        document.getElementById('txtTinhTien').innerHTML = 
+        "Họ tên: "+name+" <br>Tiền điện: "+new Intl.NumberFormat('vn-VN',).format(totalMoney)+ ' VND';
+    
+}
+
+document.getElementById('btnTinhTien').onclick = tinhTienDien;
